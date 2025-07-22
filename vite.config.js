3df663432404f8634d4615b402a44ee14e037ba6
Mohithanjan23 <pes1ug23ca060@pesu.pes.edu>
2025-07-22 22:54:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
-  // ...your other config
-};
+});
