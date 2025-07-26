559f1769348c0b0e30c007f4a6e19510988999c4
Mohithanjan23 <pes1ug23ca060@pesu.pes.edu>
2025-07-26 21:29:14 +0530

    Update tailwind.config.js

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,25 +1,8 @@
-// tailwind.config.js
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        'primary': {
-          DEFAULT: '#9D50BB',
-          light: '#A760C4',
-          dark: '#6E48AA',
-        },
-        'background': '#121212', // Main dark background
-        'surface': '#1E1E1E',   // A slightly lighter card background
-        'text-primary': '#FFFFFF',
-        'text-secondary': '#B3B3B3',
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
